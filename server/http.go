973767a5838efca5c6c1c12d93f4292abc34d637
stephen.zambaux <stephen.zambaux@gmail.com>
2018-06-26 07:23:20 +0200

    return 200 ok on / to respect Kubernetes Ingress compatibility

--- server/http.go
+++ server/http.go
@@ -191,7 +191,7 @@ func hstsHandler(handler http.Handler) http.Handler {
 }
 
 func serve404(wrt http.ResponseWriter, req *http.Request) {
-	wrt.WriteHeader(http.StatusNotFound)
+	wrt.WriteHeader(http.StatusSuccess)
 	json.NewEncoder(wrt).Encode(
 		&ServerComMessage{Ctrl: &MsgServerCtrl{
 			Timestamp: time.Now().UTC().Round(time.Millisecond),
